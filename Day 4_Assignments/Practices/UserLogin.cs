0a8eeba7bd13b28780efadf1e89d0f3d364b9532
Pavani-konduru <pavanikonduru2112@gmail.com>
2024-07-10 21:40:00 +0530

    Add files via upload

---
+++ Day 4_Assignments/Practices/UserLogin.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace UserLogib
+{
+    public class UserLogin
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the User Id : ");
+            string userId = Console.ReadLine();
+         
+           // string S = string.Format(userId, Console.ReadLine());   
+
+            Console.WriteLine("Enter the Password : ");
+            string password = Console.ReadLine();
+
+            if (userId.Trim().ToLower() == "admin" &&  password.Trim().ToLower == "admin123")
+            {
+                Console.WriteLine("Welcome to Admin");
+            }
+            else
+            {
+                Console.WriteLine("Inavlid User id or Password");
+            }
+
+        }
+    }
+}
