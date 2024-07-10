e387482fba310bcc11ef4698ba6a35f218487b2d
Pavani-konduru <pavanikonduru2112@gmail.com>
2024-07-10 15:56:57 +0530

    Add files via upload

---
+++ Day3_Assignments/Patient.cs
@@ -0,0 +1,61 @@
+﻿using System;
+namespace Patient
+{
+    class program8
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the Patieint ID Number : ");
+            int id = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the Patieint Name : ");
+            string name = (Console.ReadLine());
+
+            Console.WriteLine("is Old Patient : ");
+            bool isOld  = bool.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the Age of the Patient : ");
+            int age = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the Weight of the Patient : ");
+            double weight = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the Patient Body Temperature : ");
+            double pTemp = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the Amount  : ");
+            double amount = double.Parse(Console.ReadLine());
+
+
+            DateTime d = new DateTime();
+            Console.WriteLine("Enter the Joining Date and Time (dd/mm/yyyy) : ");
+            d = DateTime.Parse(Console.ReadLine());
+            
+
+            Console.WriteLine("-------Patient Details-------");
+
+
+            Console.WriteLine("Enter the Patieint ID Number : " + id);
+            Console.WriteLine("Enter the Patieint Name : " + name);
+            Console.WriteLine("is Old Patient : " + isOld);
+            Console.WriteLine("Enter the Age of the Patient : " + age);
+            Console.WriteLine("Enter the Weight of the Patient : " + weight);
+            Console.WriteLine("Enter the Patient Body Temperature : " + pTemp);
+            Console.WriteLine("Enter the Amount  : " + amount);
+            Console.WriteLine("Enter the Joining Date and Time : " + d);
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ Day3_Assignments/Product.cs
@@ -0,0 +1,41 @@
+﻿using System;
+namespace Product
+{
+    class program7
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the Prduct Id : ");
+            int pId = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the Prduct Name : ");
+            String pName = (Console.ReadLine());
+
+            Console.WriteLine("Enter the Unit Price: ");
+            double unitPrice = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the Quantity : ");
+            int qty = int.Parse(Console.ReadLine());
+
+            double totalAmount = unitPrice;
+            Console.WriteLine("Total Amount : " + totalAmount);
+
+            if (totalAmount > 1000 & totalAmount<3000)
+            {
+                double tenPerDis = unitPrice - (unitPrice * 0.1);
+                Console.WriteLine("After Applying 10% Discount : " + tenPerDis);
+            }
+
+            else if (totalAmount > 3000 & totalAmount<5000)
+            {
+                double fifteenPerDis = unitPrice - (unitPrice * 0.15);
+                Console.WriteLine("After Applying 15% Discount : " + fifteenPerDis);
+            }
+            else if (totalAmount > 5000)
+            {
+                double twentyPerDis = unitPrice - (unitPrice * 0.2);
+                Console.WriteLine("After Applying 20% Discount : " + twentyPerDis);
+            }
+        }
+    }
+}
