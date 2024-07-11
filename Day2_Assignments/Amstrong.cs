4d543e6ff973f922ba9adeb7e181e43a1f4bff8b
Pavani-konduru <pavanikonduru2112@gmail.com>
2024-07-11 19:57:16 +0530

    Add files via upload

---
+++ Day2_Assignments/Amstrong.cs
@@ -0,0 +1,31 @@
+﻿
+namespace FirstConsoleApp
+{
+    class Amstrong
+    {
+        public static void Main(string[] args)
+        {
+            int num, m, sum = 0, r;
+
+            Console.WriteLine("Enter the Number");
+            num = Convert.ToInt32(Console.ReadLine());
+            m= num;
+            while(m!=0)
+            {
+                r = m % 10;
+                sum = sum + (r * r * r);
+                m = m / 10;
+            }
+            if (sum == num)
+            {
+                Console.WriteLine("The Entered Number is an Amstrong Number ");
+            }
+            else
+            {
+                Console.WriteLine("The Entered Number is not an Amstrong Number");
+            }
+            }
+        }
+    }
+
+
