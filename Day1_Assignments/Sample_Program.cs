d57b96d31cae7d1eda6c2df445813146a3e19807
Pavani-konduru <pavanikonduru2112@gmail.com>
2024-07-11 19:55:04 +0530

    Add files via upload

---
+++ Day1_Assignments/Sample_Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace First_Program
+{
+    public class First_Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to India");
+
+            Console.WriteLine("Enter the Number : ");
+            int x = int.Parse(Console.ReadLine());
+
+        }
+    }
+}
