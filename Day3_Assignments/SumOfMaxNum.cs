0cc8e3e7082b4f33efeef9444d141a70fe42d85f
Pavani-konduru <pavanikonduru2112@gmail.com>
2024-07-17 18:45:26 +0530

    Add files via upload

---
+++ Day3_Assignments/SumOfMaxNum.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace SumOfMaxNum
+{
+    class SumOfMaxNum
+    {
+        public static void Main(string[] args)
+        {
+            int MaxValue1 = int.MinValue;
+            int MaxValue2 = int.MaxValue;
+            try
+            {
+                int result = checked(MaxValue1 + MaxValue2);
+                Console.WriteLine("Sum of two Integers: " + result);
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("Overflow Occured With the appropriate data type.");
+                long safeResult = (long)MaxValue1 + MaxValue2;  
+                Console.WriteLine("Safe sum of two Max integers : " + safeResult);
+
+            }
+        }
+    }
+}
